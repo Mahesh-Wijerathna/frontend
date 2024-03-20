70cff89b86aaa0529f264e5241dfdefced725160
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-03-21 04:47:26 +0530

    Revert "push"
    This reverts commit aa49a33da15bbc749320d9dbb2536713da0e3472.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
 // dependencies
+import React, { useState } from "react";
+
+import axios from "axios";
 
 // local imports
 import NavBar from "./components/Navbar";
 
 const Home = () => {
-  
+  const [destinations, setDestinations] = useState([]);
+
+  const handleSearch = async (event) => {
+    const searchTerm = event.target.value;
+
+    try {
+      const response = 
+        await axios.get(`https://destination-y1nl.onrender.com/api/v1/destination/search?name=${searchTerm}`) || 
+        await axios.get(`http://localhost:4004/api/v1/destination/search?name=${searchTerm}` ) ;
+        ;
+      console.log(response);
+      console.log(response.data);
+      console.log(
+        response.data.map((destination) => destination.name)
+      );
+      setDestinations(
+        response.data.map((destination) => destination.name)
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
@@ -12,30 +36,29 @@ const Home = () => {
       <NavBar />
       
       <br />
-      
-      
+      <h1>Search bar </h1>
+      <input type="text" placeholder="Search..." onChange={handleSearch} />
+      {destinations && destinations.length > 0 && <div className="results-list">
+        {destinations.map((result, id) => {
+        return <div
+              className="search-result"
+              onClick={(e) => alert(`You selected ${result}!`)}
+              >
+                  {result}
+              </div>  
+         
+      })}
+    </div>}   
     <button
-        style={{
-          backgroundColor: "blue",
-          color: "white",
-          padding: "10px",
-          border: "none",
-          borderRadius: "5px",
-          fontSize: "20px",
-          margin: "10px",
-          outline: "none",
-        }}
+        
         onMouseOver={(e) => e.target.style.cursor = "pointer"}
 
-        //* when the button is clicked, the user is redirected to the search page
-        onClick={(e) => {
-          window.location.href = "/search";
-        }}
-    >Search locations</button>   
+        onClick={(e) => alert("You clicked the search button!")}
+    >Search</button>   
       
 
       
-      
+      <h1>footer here</h1>
     </div>
   );
 };
